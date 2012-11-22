ecc344bfafd81667282047890e436f127519294e
Yohan Boniface <yb@enix.org>
2012-11-22 11:55:22 +0100

    Make it pip installable

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+
 from setuptools import setup, find_packages
 
+import youmap
+
+long_description = codecs.open('README.rst', "r", "utf-8").read()
+
 setup(
-    name='youmap',
-    version='1.0',
-    description="",
-    author="Lincoln Loop",
-    author_email='info@lincolnloop.com',
-    url='',
+    name="youmap",
+    version=youmap.__version__,
+    author=youmap.__author__,
+    author_email=youmap.__contact__,
+    description=youmap.__doc__,
+    keywords="django leaflet geodjango openstreetmap",
+    url=youmap.__homepage__,
+    download_url="https://bitbucket.org/yohanboniface/youmap_project/downloads",
     packages=find_packages(),
-    package_data={'youmap': ['static/*.*', 'templates/*.*']},
-    scripts=['manage.py'],
+    include_package_data=True,
+    platforms=["any"],
+    zip_safe=True,
+    long_description=long_description,
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        #"Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+    ],
 )
---
+++ youmap/__init__.py
@@ -0,0 +1,7 @@
+"Create maps with OpenStreetMap layers in a minute and embed them in your site."
+VERSION = (0, 0, 1)
+
+__author__ = 'Yohan Boniface'
+__contact__ = "yb@enix.org"
+__homepage__ = "https://bitbucket.org/yohanboniface/youmap_project"
+__version__ = ".".join(map(str, VERSION))
