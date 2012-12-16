9d2b6277e909c744edbb3688ed4ada0a33f01990
Yohan Boniface <yb@enix.org>
2012-12-16 15:10:00 +0100

    Add a basic search (using SeSQL)

--- youmap/settings/base.py
+++ youmap/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = (
     'foundation',
     'endless_pagination',
     'youmap',
+    'sesql',
 
     #'south',
 
--- youmap/views.py
+++ youmap/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
+from django.db.models import Q
+
+from sesql.shortquery import shortquery
 
 from chickpea.models import Map
 
@@ -53,3 +56,28 @@ class UserMaps(DetailView):
             return super(UserMaps, self).get_template_names()
 
 user_maps = UserMaps.as_view()
+
+
+class Search(TemplateView):
+    template_name = "youmap/search.html"
+    list_template_name = "chickpea/map_list.html"
+
+    def get_context_data(self, **kwargs):
+        q = self.request.GET['q']
+        maps = shortquery(Q(fulltext__containswords=q))
+        kwargs.update({
+            'maps': maps,
+            'q': q
+            })
+        return kwargs
+
+    def get_template_names(self):
+        """
+        Dispatch template according to the kind of request: ajax or normal.
+        """
+        if self.request.is_ajax():
+            return [self.list_template_name]
+        else:
+            return super(Search, self).get_template_names()
+
+search = Search.as_view()
