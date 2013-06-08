e3a5263c62af779ea1d2e762ff3025aa91762473
Yohan Boniface <yb@enix.org>
2013-06-08 15:40:53 +0200

    Rename category in datalayer
    cf yohanboniface/django-leaflet-storage#82

--- umap/urls.py
+++ umap/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from django.contrib import admin
+
+from leaflet_storage.views import MapShortUrl
 
 from . import views
 
@@ -13,6 +14,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'', include('social_auth.urls')),
+    # We don't want it to be localized
+    url(r'^m/(?P<pk>\d+)/$', MapShortUrl.as_view(), name='map_short_url'),
 )
 urlpatterns += i18n_patterns('',
     url(r'^$', views.home, name="home"),
