53f9a904b2fcd6d2ab6cc57c5491d534c3cd06ce
Yohan Boniface <yb@enix.org>
2013-06-21 18:02:02 +0200

    Use markdown in feature description

--- umap/settings/base.py
+++ umap/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.gis',
+    'django.contrib.markup',
 )
 
 #==============================================================================
