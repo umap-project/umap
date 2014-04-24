26e9f3974877c939046b1b6ef4e4a8a2740380e9
Yohan Boniface <yb@enix.org>
2014-04-24 23:20:25 +0200

    Better order for middleware
    See https://docs.djangoproject.com/en/1.6/topics/i18n/translation/#how-django-discovers-language-preference

--- umap/settings/base.py
+++ umap/settings/base.py
@@ -135,8 +135,13 @@ TEMPLATE_LOADERS = (
 # Middleware
 #==============================================================================
 
-MIDDLEWARE_CLASSES += (
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
 )
 
