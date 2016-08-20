805bca7a975cf36335a3112e2bb156c267c87837
Yohan Boniface <yb@enix.org>
2016-08-20 12:32:23 +0200

    Switch to pytest

--- umap/tests/test_views.py
+++ umap/tests/test_views.py
@@ -3,75 +3,79 @@ import socket
 from django.test import TestCase, RequestFactory
 from django.conf import settings
 from django.core.urlresolvers import reverse
+import pytest
 
 from umap.views import validate_url
 
 
-class TestsValidateProxyURL(TestCase):
-
-    def buildRequest(self, target="http://osm.org/georss.xml", verb="get",
-                     **kwargs):
-        defaults = {
-            'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest',
-            'HTTP_REFERER': '%s/path/' % settings.SITE_URL
-        }
-        defaults.update(kwargs)
-        func = getattr(RequestFactory(**defaults), verb)
-        return func('/', {'url': target})
-
-    def test_good_request_passes(self):
-        target = "http://osm.org/georss.xml"
-        request = self.buildRequest(target)
-        url = validate_url(request)
-        self.assertEquals(url, target)
-
-    def test_no_url_raises(self):
-        request = self.buildRequest("")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_relative_url_raises(self):
-        request = self.buildRequest("/just/a/path/")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_file_uri_raises(self):
-        request = self.buildRequest("file:///etc/passwd")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_localhost_raises(self):
-        request = self.buildRequest("http://localhost/path/")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_local_IP_raises(self):
-        url = "http://{}/path/".format(socket.gethostname())
-        request = self.buildRequest(url)
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_POST_raises(self):
-        request = self.buildRequest(verb="post")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
-
-    def test_unkown_domain_raises(self):
-        request = self.buildRequest("http://xlkjdkjsdlkjfd.com")
-        with self.assertRaises(AssertionError):
-            validate_url(request)
+def get(target="http://osm.org/georss.xml", verb="get", **kwargs):
+    defaults = {
+        'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest',
+        'HTTP_REFERER': '%s/path/' % settings.SITE_URL
+    }
+    defaults.update(kwargs)
+    func = getattr(RequestFactory(**defaults), verb)
+    return func('/', {'url': target})
 
 
-class TestsProxy(TestCase):
+def test_good_request_passes():
+    target = "http://osm.org/georss.xml"
+    request = get(target)
+    url = validate_url(request)
+    assert url == target
 
-    def test_valid_proxy_request(self):
-        url = reverse('ajax-proxy')
-        params = {'url': 'http://example.org'}
-        headers = {
-            'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest',
-            'HTTP_REFERER': settings.SITE_URL
-        }
-        response = self.client.get(url, params, **headers)
-        self.assertEquals(response.status_code, 200)
-        self.assertIn('Example Domain', response.content.decode())
-        self.assertNotIn("Cookie", response['Vary'])
+
+def test_no_url_raises():
+    request = get("")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_relative_url_raises():
+    request = get("/just/a/path/")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_file_uri_raises():
+    request = get("file:///etc/passwd")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_localhost_raises():
+    request = get("http://localhost/path/")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_local_IP_raises():
+    url = "http://{}/path/".format(socket.gethostname())
+    request = get(url)
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_POST_raises():
+    request = get(verb="post")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_unkown_domain_raises():
+    request = get("http://xlkjdkjsdlkjfd.com")
+    with pytest.raises(AssertionError):
+        validate_url(request)
+
+
+def test_valid_proxy_request(client):
+    url = reverse('ajax-proxy')
+    params = {'url': 'http://example.org'}
+    headers = {
+        'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest',
+        'HTTP_REFERER': settings.SITE_URL
+    }
+    response = client.get(url, params, **headers)
+    assert response.status_code == 200
+    assert 'Example Domain' in response.content.decode()
+    assert 'Cookie' not in response['Vary']
