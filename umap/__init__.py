eec9e094bc10c79fc081a572bedb0114670c4b0c
Yohan Boniface <yb@enix.org>
2019-02-09 10:53:45 +0100

    1.0.0

--- umap/__init__.py
+++ umap/__init__.py
@@ -1,5 +1,5 @@
 "Create maps with OpenStreetMap layers in a minute and embed them in your site."
-VERSION = (1, 0, 0, 'rc', 9)
+VERSION = (1, 0, 0)
 
 __author__ = 'Yohan Boniface'
 __contact__ = "ybon@openstreetmap.fr"
