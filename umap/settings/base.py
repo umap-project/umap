179e8c7e97aec23786cc2a4b013b7c4fe1707dae
Binnette <binnette@gmail.com>
2020-03-01 00:12:10 +0100

    Update username with value from OAuth provider

--- umap/settings/base.py
+++ umap/settings/base.py
@@ -213,5 +213,5 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details'
+    'umap.utils.user_details'
 )
