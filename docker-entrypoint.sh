0cfd7c647b8e3085cdd3e73203c1c4ef7432242d
Duvel <remco@dieselwalm.nl>
2022-10-15 17:00:02 +0200

    Prevent not displaying error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ function wait_for_database {(
   tries=0
   while true; do
     [[ $tries -lt $TRIES ]] || return
-    (echo "from django.db import connection; connection.connect()" | umap shell) >/dev/null 2>&1
+    (echo "from django.db import connection; connection.connect()" | umap shell) >/dev/null
     [[ $? -eq 0 ]] && return
     sleep $SLEEP
     tries=$((tries + 1))
