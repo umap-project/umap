a462a58ae0eed4cf5ad6c4f5a58f19c3c39186f9
Yohan Boniface <yohanboniface@free.fr>
2023-05-25 18:38:19 +0200

    Remove SOCIAL_AUTH_PIPELINE from settings/base.py
    We are using the default, so not need to define them on our
    side.

--- umap/settings/base.py
+++ umap/settings/base.py
@@ -240,15 +240,3 @@ SOCIAL_AUTH_NO_DEFAULT_PROTECTED_USER_FIELDS = True
 SOCIAL_AUTH_PROTECTED_USER_FIELDS = ("id", )
 LOGIN_URL = "login"
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/login/popup/end/"
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details'
-)
