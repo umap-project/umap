1af10f4c4c18e0037eaf995e77fce3597869d56c
Yohan Boniface <yohanboniface@free.fr>
2023-09-08 15:17:16 +0200

    Move copyToClipboard to L.Util

--- umap/static/umap/js/umap.core.js
+++ umap/static/umap/js/umap.core.js
@@ -257,6 +257,34 @@ L.Util.hasVar = (value) => {
   return typeof value === 'string' && value.indexOf('{') != -1
 }
 
+L.Util.copyToClipboard = function (textToCopy) {
+      // https://stackoverflow.com/a/65996386
+      // Navigator clipboard api needs a secure context (https)
+      if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(textToCopy)
+      } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement('textarea')
+        textArea.value = textToCopy
+
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = 'absolute'
+        textArea.style.left = '-999999px'
+
+        document.body.prepend(textArea)
+        textArea.select()
+
+        try {
+          document.execCommand('copy')
+        } catch (error) {
+          console.error(error)
+        } finally {
+          textArea.remove()
+        }
+      }
+    }
+
+
 L.DomUtil.add = (tagName, className, container, content) => {
   const el = L.DomUtil.create(tagName, className, container)
   if (content) {
--- umap/static/umap/js/umap.js
+++ umap/static/umap/js/umap.js
@@ -1173,34 +1173,6 @@ L.U.Map.include({
     formData.append('name', this.options.name)
     formData.append('center', JSON.stringify(this.geometry()))
     formData.append('settings', JSON.stringify(geojson))
-
-    function copyToClipboard(textToCopy) {
-      // https://stackoverflow.com/a/65996386
-      // Navigator clipboard api needs a secure context (https)
-      if (navigator.clipboard && window.isSecureContext) {
-        navigator.clipboard.writeText(textToCopy)
-      } else {
-        // Use the 'out of viewport hidden text area' trick
-        const textArea = document.createElement('textarea')
-        textArea.value = textToCopy
-
-        // Move textarea out of the viewport so it's not visible
-        textArea.style.position = 'absolute'
-        textArea.style.left = '-999999px'
-
-        document.body.prepend(textArea)
-        textArea.select()
-
-        try {
-          document.execCommand('copy')
-        } catch (error) {
-          console.error(error)
-        } finally {
-          textArea.remove()
-        }
-      }
-    }
-
     this.post(this.getSaveUrl(), {
       data: formData,
       context: this,
@@ -1232,7 +1204,7 @@ L.U.Map.include({
               {
                 label: L._('Copy link'),
                 callback: () => {
-                  copyToClipboard(data.permissions.anonymous_edit_url)
+                  L.Util.copyToClipboard(data.permissions.anonymous_edit_url)
                   this.ui.alert({
                     content: L._('Secret edit link copied to clipboard!'),
                     level: 'info',
