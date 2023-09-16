0c9d9f630e0a5ee8354e2453efd1a854dfa0e1c4
Brian DeRocher <brian@derocher.org>
2023-09-16 11:12:50 -0400

    Replace wait_for_database with depends_on and healthcheck.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,24 +3,6 @@ set -eo pipefail
 
 source /venv/bin/activate
 
-# default variables
-: "${SLEEP:=1}"
-: "${TRIES:=60}"
-
-function wait_for_database {(
-  echo "Waiting for database to respond..."
-  tries=0
-  while true; do
-    [[ $tries -lt $TRIES ]] || return
-    (echo "from django.db import connection; connection.connect()" | umap shell) >/dev/null
-    [[ $? -eq 0 ]] && return
-    sleep $SLEEP
-    tries=$((tries + 1))
-  done
-)}
-
-# first wait for the database
-wait_for_database
 # then migrate the database
 umap migrate
 # then collect static files
