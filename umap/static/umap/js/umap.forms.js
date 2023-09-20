35e37c2d4bea2f557f9c74d49d74fc7d40b6a6ab
Brian DeRocher <brian@derocher.org>
2023-09-20 10:52:13 -0400

    Add translation for "owner".

--- umap/static/umap/js/umap.forms.js
+++ umap/static/umap/js/umap.forms.js
@@ -940,7 +940,7 @@ L.U.FormBuilder = L.FormBuilder.extend({
   defaultOptions: {
     name: { label: L._('name') },
     owner: {
-      label: 'owner',
+      label: L._('owner'),
 //      handler: 'BlurInput',  // this field should be hidden, not sure if blur does it.
     },
     description: {
