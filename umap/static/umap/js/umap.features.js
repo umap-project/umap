f05bdb2ac34b6b4339183fc46213ca1f78025d5d
Yohan Boniface <yohanboniface@free.fr>
2023-09-20 09:54:06 +0200

    Use datalayer.isDataReadOnly in feature.isReadOnly

--- umap/static/umap/js/umap.features.js
+++ umap/static/umap/js/umap.features.js
@@ -40,7 +40,7 @@ L.U.FeatureMixin = {
   preInit: function () {},
 
   isReadOnly: function () {
-    return this.datalayer && (this.datalayer.isRemoteLayer() || this.datalayer.isReadOnly())
+    return this.datalayer && this.datalayer.isDataReadOnly()
   },
 
   getSlug: function () {
