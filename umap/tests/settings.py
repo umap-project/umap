656e29c72b8bd478dfde1734b8686df727144ad2
David Larlet <david@larlet.fr>
2024-01-16 17:41:38 -0500

    Use custom storage instead of django-compressor

--- umap/tests/settings.py
+++ umap/tests/settings.py
@@ -3,7 +3,6 @@ import os
 from umap.settings.base import *  # pylint: disable=W0614,W0401
 
 SECRET_KEY = "justfortests"
-COMPRESS_ENABLED = False
 FROM_EMAIL = "test@test.org"
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
