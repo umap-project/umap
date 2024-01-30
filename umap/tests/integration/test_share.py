4a99f357f2ea8076bbc51fdcf9be20ac61d8e483
Yohan Boniface <yohanboniface@free.fr>
2024-01-30 19:38:42 +0100

    chore: lint

--- umap/tests/integration/test_share.py
+++ umap/tests/integration/test_share.py
@@ -13,10 +13,10 @@ def test_iframe_code(map, live_server, datalayer, page):
     expect(textarea).to_have_text(re.compile('src="'))
     expect(textarea).to_have_text(re.compile('href="'))
     # We should ave both, once for iframe link, once for full screen
-    expect(textarea).to_have_text(re.compile('scrollWheelZoom=true'))
-    expect(textarea).to_have_text(re.compile('scrollWheelZoom=false'))
-    expect(textarea).not_to_have_text(re.compile(f'datalayers={datalayer.pk}'))
+    expect(textarea).to_have_text(re.compile("scrollWheelZoom=true"))
+    expect(textarea).to_have_text(re.compile("scrollWheelZoom=false"))
+    expect(textarea).not_to_have_text(re.compile(f"datalayers={datalayer.pk}"))
     # Open options
     page.get_by_text("Embed and link options").click()
     page.get_by_title("Keep current visible layers").click()
-    expect(textarea).to_have_text(re.compile(f'datalayers={datalayer.pk}'))
+    expect(textarea).to_have_text(re.compile(f"datalayers={datalayer.pk}"))
