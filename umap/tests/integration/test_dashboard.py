434a2928780149b9561fecd61113be684b931787
Yohan Boniface <yohanboniface@free.fr>
2024-02-16 12:36:05 +0100

    fix: dashboard preview was still calling L.U.

--- umap/tests/integration/test_dashboard.py
+++ umap/tests/integration/test_dashboard.py
@@ -23,3 +23,16 @@ def test_owner_can_delete_map_after_confirmation(map, live_server, login):
         delete_button.click()
     assert dialog_shown
     assert Map.objects.all().count() == 0
+
+
+def test_dashboard_map_preview(map, live_server, datalayer, login):
+    page = login(map.owner)
+    page.goto(f"{live_server.url}/en/me")
+    dialog = page.locator("dialog")
+    expect(dialog).to_be_hidden()
+    button = page.get_by_role("button", name="Open preview")
+    expect(button).to_be_visible()
+    button.click()
+    expect(dialog).to_be_visible()
+    # Let's check we have a marker on it, so we can guess the map loaded correctly
+    expect(dialog.locator(".leaflet-marker-icon")).to_be_visible()
