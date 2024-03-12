09417070c8e6f027513efafd9d37c77b65b05935
Yohan Boniface <yohanboniface@free.fr>
2024-03-12 11:32:05 +0100

    chore: refactor browser back button

--- umap/static/umap/js/umap.layer.js
+++ umap/static/umap/js/umap.layer.js
@@ -1363,7 +1363,11 @@ U.DataLayer = L.Evented.extend({
         '_blank'
       )
     }
-    this.map.ui.openPanel({ data: { html: container }, className: 'dark' })
+    this.map.ui.openPanel({
+      data: { html: container },
+      className: 'dark',
+      actions: [U.Browser.backButton(this.map)],
+    })
   },
 
   getOwnOption: function (option) {
