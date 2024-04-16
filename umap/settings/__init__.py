40d226e3eadc5fa96be333a641a62be3218cef55
Alexis Métaireau <alexis@notmyidea.org>
2024-04-16 18:07:35 +0200

    feat(settings): Expose settings as a dict.
    This makes it possible to use them in standalone scripts, when using
    `django.settings.configure(**settings_dict)`.

--- umap/settings/__init__.py
+++ umap/settings/__init__.py
@@ -43,3 +43,6 @@ if path:
                     globals()["STATICFILES_DIRS"].insert(0, value)
                 else:
                     globals()[key] = value
+
+# Expose these settings for consumption by e.g. django.settings.configure.
+settings_as_dict = {k: v for k, v in globals().items() if k.isupper()}
