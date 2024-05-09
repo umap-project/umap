1f0782665bb0b3aa40c3be7677423585edebcc5e
Alexis Métaireau <alexis@notmyidea.org>
2024-05-09 18:04:54 +0200

    feat(sync): handle syncing of map limitbounds.

--- umap/static/umap/js/umap.js
+++ umap/static/umap/js/umap.js
@@ -1445,6 +1445,14 @@ U.Map = L.Map.extend({
         this.options.limitBounds.north = L.Util.formatNum(bounds.getNorth())
         this.options.limitBounds.east = L.Util.formatNum(bounds.getEast())
         boundsBuilder.fetchAll()
+
+        const { subject, metadata, engine } = this.getSyncMetadata()
+        engine.update(
+          subject,
+          metadata,
+          'options.limitBounds',
+          this.options.limitBounds
+        )
         this.isDirty = true
         this.handleLimitBounds()
       },
