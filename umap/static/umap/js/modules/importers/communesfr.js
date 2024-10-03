f1619579a48b08a99e05a3501688c8e01fc6c65f
Yohan Boniface <yohanboniface@free.fr>
2024-10-03 16:55:02 +0200

    feat: allow to search by code INSEE in communes importer

--- umap/static/umap/js/modules/importers/communesfr.js
+++ umap/static/umap/js/modules/importers/communesfr.js
@@ -1,5 +1,6 @@
 import { DomUtil } from '../../../vendors/leaflet/leaflet-src.esm.js'
 import { BaseAjax, SingleMixin } from '../autocomplete.js'
+import * as Util from '../utils.js'
 
 class Autocomplete extends SingleMixin(BaseAjax) {
   createResult(item) {
@@ -7,6 +8,17 @@ class Autocomplete extends SingleMixin(BaseAjax) {
       value: item.code,
       label: `${item.nom} (${item.code})`,
     })
+  }
+
+  buildUrl(value) {
+    let url = this.url
+    let options = { q: encodeURIComponent(value) }
+    const re = /^(0[1-9]|[1-9][ABab\d])\d{3}$/gm
+    if (re.test(value)) {
+      url = "https://geo.api.gouv.fr/communes?code={code}&limit=5"
+      options = { code: encodeURIComponent(value) }
+    }
+    return Util.template(url, options)
   }
 }
 
@@ -25,7 +37,7 @@ export class Importer {
       textContent: "Importer les contours d'une commune française.",
     })
     const options = {
-      placeholder: 'Commune…',
+      placeholder: 'Nom ou code INSEE…',
       url: 'https://geo.api.gouv.fr/communes?nom={q}&limit=5',
       on_select: (choice) => {
         importer.url = `https://geo.api.gouv.fr/communes?code=${choice.item.value}&format=geojson&geometry=contour`
