c74971bc0ed4d58728bac84a3263f0fe301ba523
Yohan Boniface <yohanboniface@free.fr>
2024-10-21 11:50:10 +0200

    chore: move pydantic and websocket in sync dependencies target

--- umap/asgi.py
+++ umap/asgi.py
@@ -11,6 +11,5 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        # Just HTTP for now. (We can add other protocols later.)
     }
 )
