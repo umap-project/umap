umap-project/umap

commit 4adc5585606d8f14e53523048034331fe44c2092

chore: use our contexmenu class for inplace toolbar
And remove Leaflet.Toolbar dependency.
This also teach ContextMenu how to display icons instead of
text and how to render in horizontal orientation instead of
vertical.

umap/static/umap/js/modules/rendering/ui.js
@@ -64,11 +64,10 @@ const FeatureMixin = {
           else this.feature.edit(event)
         }
       } else if (!this._map.editTools?.drawing()) {
-        new L.Toolbar.Popup(event.latlng, {
-          className: 'leaflet-inplace-toolbar',
-          anchor: this.getPopupToolbarAnchor(),
-          actions: this.feature.getInplaceToolbarActions(event),
-        }).addTo(this._map, this.feature, event.latlng)
+        this._map._umap.editContextmenu.open(
+          event.originalEvent,
+          this.feature.getInplaceEditMenu(event)
+        )
       }
     }
     DomEvent.stop(event)
@@ -105,8 +104,6 @@ const FeatureMixin = {
     this.feature.pullGeometry(false)
     this.feature.onCommit()
   },
-
-  getPopupToolbarAnchor: () => [0, 0],
 }
 
 const PointMixin = {
@@ -248,10 +245,6 @@ export const LeafletMarker = Marker.extend({
     this._redraw()
     this._resetZIndex()
   },
-
-  getPopupToolbarAnchor: function () {
-    return this.options.icon.options.popupAnchor
-  },
 })
 
 const PathMixin = {
@@ -341,13 +334,11 @@ const PathMixin = {
     this.resetTooltip()
   },
 
-  getVertexActions: () => [U.DeleteVertexAction],
-
   onVertexRawClick: function (event) {
-    new L.Toolbar.Popup(event.latlng, {
-      className: 'leaflet-inplace-toolbar',
-      actions: this.getVertexActions(event),
-    }).addTo(this._map, this, event.latlng, event.vertex)
+    this._map._umap.editContextmenu.open(
+      event.originalEvent,
+      this.feature.getInplaceEditVertexMenu(event)
+    )
   },
 
   isolateShape: function (atLatLng) {
@@ -382,17 +373,6 @@ export const LeafletPolyline = Polyline.extend({
   includes: [FeatureMixin, PathMixin],
 
   getClass: () => LeafletPolyline,
-
-  getVertexActions: function (event) {
-    const actions = PathMixin.getVertexActions.call(this, event)
-    const index = event.vertex.getIndex()
-    if (index === 0 || index === event.vertex.getLastIndex()) {
-      actions.push(U.ContinueLineAction)
-    } else {
-      actions.push(U.SplitLineAction)
-    }
-    return actions
-  },
 
   getMeasure: function (shape) {
     let shapes

scripts/vendorsjs.sh
@@ -16,7 +16,6 @@ mkdir -p umap/static/umap/vendors/markercluster/ && cp -r node_modules/leaflet.m
 mkdir -p umap/static/umap/vendors/markercluster/ && cp -r node_modules/leaflet.markercluster/dist/MarkerCluster.* umap/static/umap/vendors/markercluster/
 mkdir -p umap/static/umap/vendors/heat/ && cp -r node_modules/leaflet.heat/dist/leaflet-heat.js umap/static/umap/vendors/heat/
 mkdir -p umap/static/umap/vendors/fullscreen/ && cp -r node_modules/leaflet-fullscreen/dist/** umap/static/umap/vendors/fullscreen/
-mkdir -p umap/static/umap/vendors/toolbar/ && cp -r node_modules/leaflet-toolbar/dist/leaflet.toolbar.* umap/static/umap/vendors/toolbar/
 mkdir -p umap/static/umap/vendors/measurable/ && cp -r node_modules/leaflet-measurable/Leaflet.Measurable.* umap/static/umap/vendors/measurable/
 mkdir -p umap/static/umap/vendors/photon/ && cp -r node_modules/leaflet.photon/leaflet.photon.js umap/static/umap/vendors/photon/
 mkdir -p umap/static/umap/vendors/csv2geojson/ && cp -r node_modules/csv2geojson/csv2geojson.js umap/static/umap/vendors/csv2geojson/

umap/static/umap/css/contextmenu.css
@@ -1,12 +1,20 @@
 .umap-contextmenu {
     background-color: var(--background-color);
-    padding: calc(var(--box-padding) / 2) var(--box-padding);
+    padding: calc(var(--box-padding) / 2) calc(var(--box-padding) / 2);
     position: fixed;
     z-index: var(--zindex-contextmenu);
     border-radius: var(--border-radius);
     box-shadow: var(--block-shadow);
+    display: flex;
+    flex-direction: column;
 }
-.umap-contextmenu li + li {
+.umap-contextmenu-rows {
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+}
+.umap-contextmenu:not(.umap-contextmenu-rows) li + li {
     margin-top: var(--text-margin);
 }
 

umap/static/umap/css/icon.css
@@ -68,11 +68,20 @@ html[dir="rtl"] .icon {
 .icon-close {
     background-position: var(--tile) 0px;
 }
+.icon-continue-line {
+    background-position: calc(var(--tile) * 4) 0;
+}
 .icon-copy {
     background-position: calc(var(--tile) * 2) calc(var(--tile) * 4);
 }
 .icon-delete {
     background-position: calc(var(--tile) * 5) calc(var(--tile) * 2);
+}
+.icon-delete-shape {
+    background-position: calc(var(--tile) * 4) calc(var(--tile) * 3);
+}
+.icon-delete-vertex {
+    background-position: calc(var(--tile) * 5) calc(var(--tile) * 4);
 }
 .readonly .icon-delete,
 .off .icon-delete {
@@ -98,6 +107,9 @@ html[dir="rtl"] .icon {
 .off .icon-edit {
     background-position: calc(var(--tile) * 2) calc(var(--tile) * 3);
 }
+.icon-extract-shape {
+    background-position: calc(var(--tile) * 5) 0;
+}
 .icon-filters {
     background-position: 0px var(--tile);
 }
@@ -106,6 +118,9 @@ html[dir="rtl"] .icon {
 }
 .icon-help {
     background-position: 0 0;
+}
+.icon-hole {
+    background-position: calc(var(--tile) * 3) calc(var(--tile) * 4);
 }
 .icon-key {
     background-position: calc(var(--tile) * 6) calc(var(--tile) * 5);
@@ -164,6 +179,9 @@ html[dir="rtl"] .icon {
 }
 .icon-share {
     background-position: 0px calc(var(--tile) * 5);
+}
+.icon-split-line {
+    background-position: calc(var(--tile) * 5) var(--tile);
 }
 .icon-star {
     background-position: var(--tile) calc(var(--tile) * 7);

umap/static/umap/js/modules/data/features.js
@@ -270,6 +270,10 @@ class Feature {
     if (!this.ui.isOnScreen(this._umap._leafletMap.getBounds())) this.zoomTo(event)
   }
 
+  toggleEditing() {
+    this.edit()
+  }
+
   getAdvancedEditActions(container) {
     const button = Utils.loadTemplate(`
       <button class="button" type="button">
@@ -508,8 +512,19 @@ class Feature {
     })
   }
 
-  getInplaceToolbarActions() {
-    return [U.ToggleEditAction, U.DeleteFeatureAction]
+  getInplaceEditMenu() {
+    return [
+      {
+        action: () => this.toggleEditing(),
+        title: translate('Toggle edit mode (⇧+Click)'),
+        icon: 'icon-edit',
+      },
+      {
+        action: () => this.confirmDelete(),
+        title: translate('Delete this feature'),
+        icon: 'icon-delete',
+      },
+    ]
   }
 
   isFiltered() {
@@ -785,7 +800,7 @@ class Path extends Feature {
     }
   }
 
-  _toggleEditing(event) {
+  toggleEditing(event) {
     if (this._umap.editEnabled) {
       if (this.ui.editEnabled()) {
         this.endEdit()
@@ -795,7 +810,7 @@ class Path extends Feature {
       }
     }
     // FIXME: disable when disabling global edit
-    L.DomEvent.stop(event)
+    // L.DomEvent.stop(event)
   }
 
   getShapeOptions() {
@@ -846,13 +861,31 @@ class Path extends Feature {
     return other
   }
 
-  getInplaceToolbarActions(event) {
-    const items = super.getInplaceToolbarActions(event)
+  getInplaceEditMenu(event) {
+    const items = super.getInplaceEditMenu()
     if (this.isMulti()) {
-      items.push(U.DeleteShapeAction)
-      items.push(U.ExtractShapeFromMultiAction)
+      items.push({
+        action: () => this.ui.enableEdit().deleteShapeAt(event.latlng),
+        title: translate('Delete this shape'),
+        icon: 'icon-delete-shape',
+      })
+      items.push({
+        action: () => this.ui.isolateShape(event.latlng),
+        title: translate('Extract shape to separate feature'),
+        icon: 'icon-extract-shape',
+      })
     }
     return items
+  }
+
+  getInplaceEditVertexMenu(event) {
+    return [
+      {
+        action: () => event.vertex.delete(),
+        title: translate('Delete this vertex (Alt+Click)'),
+        icon: 'icon-delete-vertex',
+      },
+    ]
   }
 
   zoomTo({ easing, callback }) {
@@ -1077,6 +1110,25 @@ export class LineString extends Path {
     const [gain, loss] = this.ui.getElevation()
     return Object.assign({ gain, loss }, super.extendedProperties())
   }
+
+  getInplaceEditVertexMenu(event) {
+    const items = super.getInplaceEditVertexMenu(event)
+    const index = event.vertex.getIndex()
+    if (index === 0 || index === event.vertex.getLastIndex()) {
+      items.push({
+        action: () => event.vertex.continue(),
+        title: translate('Continue line'),
+        icon: 'icon-continue-line',
+      })
+    } else {
+      items.push({
+        action: () => event.vertex.split(),
+        title: translate('Split line'),
+        icon: 'icon-split-line',
+      })
+    }
+    return items
+  }
 }
 
 export class Polygon extends Path {
@@ -1182,9 +1234,13 @@ export class Polygon extends Path {
     )
   }
 
-  getInplaceToolbarActions(event) {
-    const items = super.getInplaceToolbarActions(event)
-    items.push(U.CreateHoleAction)
+  getInplaceEditMenu(event) {
+    const items = super.getInplaceEditMenu()
+    items.push({
+      action: () => this.ui.startHole(event),
+      title: translate('Start a hole here'),
+      icon: 'icon-hole',
+    })
     return items
   }
 

umap/static/umap/js/modules/rendering/map.js
@@ -250,7 +250,6 @@ export const LeafletMap = BaseMap.extend({
       DomEvent.on(document.body, 'dataload', (event) =>
         this.fire('dataload', event.detail)
       )
-      this.on('click', this.closeInplaceToolbar)
     }
 
     this.on('baselayerchange', (e) => {
@@ -282,11 +281,6 @@ export const LeafletMap = BaseMap.extend({
     // Needs tilelayer to exist for minimap
     this.renderControls()
     this.handleLimitBounds()
-  },
-
-  closeInplaceToolbar: function () {
-    const toolbar = this._toolbars[L.Toolbar.Popup._toolbar_class_id]
-    if (toolbar) toolbar.remove()
   },
 
   latLng: (a, b, c) => {

umap/static/umap/js/modules/ui/contextmenu.js
@@ -10,6 +10,9 @@ export default class ContextMenu extends Positioned {
     if (options.className) {
       this.container.classList.add(options.className)
     }
+    if (options.orientation === 'rows') {
+      this.container.classList.add('umap-contextmenu-rows')
+    }
     this.container.addEventListener('focusout', (event) => {
       if (!this.container.contains(event.relatedTarget)) this.close()
     })
@@ -37,10 +40,14 @@ export default class ContextMenu extends Positioned {
         )
         this.container.appendChild(li)
       } else {
+        let content = item.label || ''
+        if (item.icon) {
+          content = `<i class="icon icon-16 ${item.icon}"></i>${content}`
+        }
         const li = loadTemplate(
-          `<li class="${item.className || ''}"><button tabindex="0" class="flat">${item.label}</button></li>`
+          `<li class="${item.className || ''}"><button tabindex="0" class="flat" title="${item.title || ''}">${content}</button></li>`
         )
-        li.addEventListener('click', () => {
+        li.firstChild.addEventListener('click', () => {
           this.close()
           item.action()
         })

umap/static/umap/js/modules/umap.js
@@ -120,6 +120,7 @@ export default class Umap extends ServerStored {
     )
     this.tooltip = new Tooltip(this._leafletMap._controlContainer)
     this.contextmenu = new ContextMenu()
+    this.editContextmenu = new ContextMenu({ className: 'dark', orientation: 'rows' })
     this.server = new ServerRequest()
     this.request = new Request()
     this.facets = new Facets(this)
@@ -1299,7 +1300,6 @@ export default class Umap extends ServerStored {
     this.editPanel.close()
     this.fullPanel.close()
     this.sync.stop()
-    this._leafletMap.closeInplaceToolbar()
   }
 
   fire(name) {

commit ba20f5791cf0e340a6a10fdfeecfdb7c5caeb6d7

fix: shift-click on a path should toggle edit
This was broken in 4adc5585606d8f14e53523048034331fe44c2092

Co-authored-by: David Larlet <david@larlet.fr>

umap/static/umap/js/modules/rendering/ui.js
@@ -60,8 +60,7 @@ const FeatureMixin = {
         if (event.originalEvent.ctrlKey || event.originalEvent.metaKey) {
           this.feature.datalayer.edit(event)
         } else {
-          if (this.feature._toggleEditing) this.feature._toggleEditing(event)
-          else this.feature.edit(event)
+          this.feature.toggleEditing(event)
         }
       } else if (!this._map.editTools?.drawing()) {
         this._map._umap.editContextmenu.open(