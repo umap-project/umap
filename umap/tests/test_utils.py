9781e8b9d1852d7a6ed8eb6b0c11c3e0639a2dde
Yohan Boniface <yohanboniface@free.fr>
2025-11-04 18:15:00 +0100

    fix: honour settings.FILE_UPLOAD_PERMISSIONS when creating gzip files
    fix #3044
    fix #2852
    
    Co-authored-by: David Larlet <david@larlet.fr>

--- umap/tests/test_utils.py
+++ umap/tests/test_utils.py
@@ -1,3 +1,4 @@
+import stat
 from pathlib import Path
 
 import pytest
@@ -5,7 +6,8 @@ import pytest
 from umap.utils import gzip_file, normalize_string
 
 
-def test_gzip_file():
+def test_gzip_file(settings):
+    settings.FILE_UPLOAD_PERMISSIONS = 0o666
     # Let's use any old file so we can check that the date of the gzip file is set.
     src = Path(__file__).parent / "settings.py"
     dest = Path("/tmp/test_settings.py.gz")
@@ -14,6 +16,7 @@ def test_gzip_file():
     dest_stat = dest.stat()
     dest.unlink()
     assert src_stat.st_mtime == dest_stat.st_mtime
+    assert stat.filemode(dest_stat.st_mode) == "-rw-rw-rw-"
 
 
 @pytest.mark.parametrize(
