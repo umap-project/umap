b98fed93431c00b56675b12381cc88aecb119205
Yohan Boniface <yohanboniface@free.fr>
2026-01-07 19:00:16 +0100

    feat: add management command to search, delete, block and restore maps
    fix #3155

---
+++ umap/tests/test_search_maps_command.py
@@ -0,0 +1,34 @@
+import pytest
+from django.core.management import call_command
+
+from umap.models import Map
+
+from .base import MapFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_search_and_delete_maps(map, team):
+    target = MapFactory(name="find me")
+    assert Map.objects.filter(share_status=Map.DELETED).count() == 0
+
+    call_command("search_maps", "find", "--delete", "--dry-run", "--no-input")
+    assert Map.objects.filter(share_status=Map.DELETED).count() == 0
+
+    call_command("search_maps", "find", "--delete", "--no-input")
+    assert Map.objects.filter(share_status=Map.DELETED).count() == 1
+
+    assert not Map.public.filter(pk=target.pk)
+
+
+def test_search_and_block_maps(map, team):
+    target = MapFactory(name="find me")
+    assert Map.objects.filter(share_status=Map.BLOCKED).count() == 0
+
+    call_command("search_maps", "find", "--block", "--dry-run", "--no-input")
+    assert Map.objects.filter(share_status=Map.BLOCKED).count() == 0
+
+    call_command("search_maps", "find", "--block", "--no-input")
+    assert Map.objects.filter(share_status=Map.BLOCKED).count() == 1
+
+    assert not Map.public.filter(pk=target.pk)
