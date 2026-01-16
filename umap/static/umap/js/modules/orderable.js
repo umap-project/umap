e36a1c16eab868ff98695b01289944f8972d7f47
Yohan Boniface <yohanboniface@free.fr>
2026-01-16 12:10:49 +0100

    wip: naive version of tree capable drag'n'drop

--- umap/static/umap/js/modules/orderable.js
+++ umap/static/umap/js/modules/orderable.js
@@ -1,23 +1,22 @@
-import { DomEvent } from '../../vendors/leaflet/leaflet-src.esm.js'
-
 export default class Orderable {
-  constructor(parent, onDrop, selector = '.orderable') {
+  constructor(parent, onDrop) {
     this.parent = parent
     this.onCommit = onDrop
     this.src = null
     this.dst = null
-    this.els = this.parent.querySelectorAll(selector)
-    for (let i = 0; i < this.els.length; i++) this.makeDraggable(this.els[i])
+    for (const node of this.parent.querySelectorAll('.orderable')) {
+      this.makeDraggable(node)
+    }
   }
 
   makeDraggable(node) {
     node.draggable = true
-    DomEvent.on(node, 'dragstart', this.onDragStart, this)
-    DomEvent.on(node, 'dragenter', this.onDragEnter, this)
-    DomEvent.on(node, 'dragover', this.onDragOver, this)
-    DomEvent.on(node, 'dragleave', this.onDragLeave, this)
-    DomEvent.on(node, 'drop', this.onDrop, this)
-    DomEvent.on(node, 'dragend', this.onDragEnd, this)
+    node.addEventListener('dragstart', (event) => this.onDragStart(event))
+    node.addEventListener('dragenter', (event) => this.onDragEnter(event))
+    node.addEventListener('dragover', (event) => this.onDragOver(event))
+    node.addEventListener('dragleave', (event) => this.onDragLeave(event))
+    node.addEventListener('drop', (event) => this.onDrop(event))
+    node.addEventListener('dragend', (event) => this.onDragEnd(event))
   }
 
   nodeIndex(node) {
@@ -26,59 +25,68 @@ export default class Orderable {
 
   findTarget(node) {
     while (node) {
-      if (this.nodeIndex(node) !== -1) return node
+      if (node.classList.contains('orderable')) return node
       node = node.parentNode
     }
   }
 
-  onDragStart(e) {
-    // e.target is the source node.
-    const realSrc = document.elementFromPoint(e.clientX, e.clientY)
+  onDragStart(event) {
+    // event.target is the source nodevent.
+    const realSrc = document.elementFromPoint(event.clientX, event.clientY)
     // Only allow drag from the handle
     if (!realSrc.classList.contains('icon-drag')) {
-      e.preventDefault()
+      event.preventDefault()
       return
     }
-    this.src = e.target
+    this.src = event.target
     this.initialIndex = this.nodeIndex(this.src)
     this.src.classList.add('ordering')
-    e.dataTransfer.effectAllowed = 'move'
-    e.dataTransfer.setData('text/html', this.src.innerHTML)
+    event.dataTransfer.effectAllowed = 'move'
+    event.dataTransfer.setData('text/html', this.src.innerHTML)
   }
 
-  onDragOver(e) {
-    DomEvent.stop(e)
-    if (e.preventDefault) e.preventDefault() // Necessary. Allows us to drop.
-    e.dataTransfer.dropEffect = 'move'
+  onDragOver(event) {
+    event.stopPropagation()
+    event.preventDefault() // Necessary. Allows us to drop.
+    event.dataTransfer.dropEffect = 'move'
     return false
   }
 
-  onDragEnter(e) {
-    DomEvent.stop(e)
-    // e.target is the current hover target.
-    const dst = this.findTarget(e.target)
+  onDragEnter(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    // event.target is the current hover target.
+    const dst = this.findTarget(event.target)
     if (!dst || dst === this.src) return
     this.dst = dst
     const targetIndex = this.nodeIndex(this.dst)
     const srcIndex = this.nodeIndex(this.src)
-    if (targetIndex > srcIndex) this.parent.insertBefore(this.dst, this.src)
-    else this.parent.insertBefore(this.src, this.dst)
+    const parentNode = this.dst.parentNode
+    if (targetIndex > srcIndex) {
+      if (this.dst.nextSibling) {
+        parentNode.insertBefore(this.src, this.dst.nextSibling)
+      } else {
+        parentNode.appendChild(this.src)
+      }
+    } else {
+      parentNode.insertBefore(this.src, this.dst)
+    }
   }
 
-  onDragLeave(e) {
-    // e.target is previous target element.
+  onDragLeave(event) {
+    // event.target is previous target element.
   }
 
-  onDrop(e) {
-    // e.target is current target element.
-    if (e.stopPropagation) e.stopPropagation() // Stops the browser from redirecting.
+  onDrop(event) {
+    // event.target is current target element.
+    if (event.stopPropagation) event.stopPropagation() // Stops the browser from redirecting.
     if (!this.dst) return
     this.onCommit(this.src, this.dst, this.initialIndex, this.nodeIndex(this.src))
     return false
   }
 
-  onDragEnd(e) {
-    // e.target is the source node.
+  onDragEnd(event) {
+    // event.target is the source node.
     this.src.classList.remove('ordering')
   }
 }
