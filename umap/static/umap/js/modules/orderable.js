b111887247400b87b5102aacc6bd2289995ea38c
Yohan Boniface <yohanboniface@free.fr>
2026-01-30 17:26:26 +0100

    wip: deal with notree and nochildren
    notree: we do not allow at all to have children node
            during drag'n'drop (eg. for rules)
    nochildren: we do not allow to make this node child of
                this given node (eg. for a layer which has features)
    
    Co-authored-by: David Larlet <david@larlet.fr>

--- umap/static/umap/js/modules/orderable.js
+++ umap/static/umap/js/modules/orderable.js
@@ -1,9 +1,10 @@
 export default class Orderable {
-  constructor(parent, onDrop) {
+  constructor(parent, onDrop, properties) {
+    this.properties = Object.assign({}, properties)
     this.parent = parent
     this.onCommit = onDrop
-    this.src = null
-    this.dst = null
+    this.moved = null
+    this.target = null
     for (const node of this.parent.querySelectorAll('.orderable')) {
       this.makeDraggable(node)
     }
@@ -34,61 +35,68 @@ export default class Orderable {
     el.classList.remove('target-above')
     el.classList.remove('target-middle')
     el.classList.remove('target-below')
+    el.classList.remove('target-not-allowed')
   }
 
   onDragStart(event) {
     // event.target is the source nodevent.
-    const realSrc = document.elementFromPoint(event.clientX, event.clientY)
+    const handle = document.elementFromPoint(event.clientX, event.clientY)
     // Only allow drag from the handle
-    if (!realSrc.classList.contains('icon-drag')) {
+    if (!handle.classList.contains('icon-drag')) {
       event.preventDefault()
       return
     }
-    this.src = event.target
-    if (!this.src) return
-    this.initialIndex = this.nodeIndex(this.src)
-    this.src.classList.add('ordering')
+    this.moved = event.target
+    if (!this.moved) return
+    this.initialIndex = this.nodeIndex(this.moved)
+    this.moved.classList.add('ordering')
   }
 
   onDragOver(event) {
     event.stopPropagation()
     event.preventDefault() // Necessary. Allows us to drop.
     event.dataTransfer.dropEffect = 'move'
-    const dst = this.findTarget(event.target)
-    if (!dst || dst === this.src) return false
+    const target = this.findTarget(event.target)
+    if (!target || target === this.moved || this.moved.contains(target)) return false
     this.pointerY = event.clientY
-    this.dst = dst
-    const top = dst.getBoundingClientRect().top
-    const bottom = dst.getBoundingClientRect().bottom
+    this.target = target
+    const top = target.getBoundingClientRect().top
+    const bottom = target.getBoundingClientRect().bottom
     const height = bottom - top
     const third = height / 3
+    this.dragMode = undefined
     if (this.pointerY < top + third) {
       this.dragMode = 'above'
     } else if (this.pointerY > bottom - third) {
       this.dragMode = 'below'
+    } else if (!this.target.classList.contains('no-children')) {
+      if (this.properties.allowTree) {
+        this.dragMode = 'middle'
+      }
     } else {
-      this.dragMode = 'middle'
+      this.dragMode = 'not-allowed'
     }
-    this.resetCSS(dst)
-    dst.classList.add(`target-${this.dragMode}`)
-    this.src.classList.remove('drageffect')
+    this.resetCSS(target)
+    target.classList.add(`target-${this.dragMode}`)
+    if (this.dragMode === 'not-allowed') return
+    this.moved.classList.remove('drageffect')
 
     window.setTimeout(() => {
       if (this.pointerY !== event.clientY) return
-      this.src.classList.add('drageffect')
-      const parentNode = dst.parentNode
+      this.moved.classList.add('drageffect')
+      const parentNode = target.parentNode
       if (this.dragMode === 'above') {
-        parentNode.insertBefore(this.src, this.dst)
+        parentNode.insertBefore(this.moved, this.target)
       } else if (this.dragMode === 'below') {
-        if (this.dst.nextSibling) {
-          parentNode.insertBefore(this.src, this.dst.nextSibling)
+        if (this.target.nextSibling) {
+          parentNode.insertBefore(this.moved, this.target.nextSibling)
         } else {
-          parentNode.appendChild(this.src)
+          parentNode.appendChild(this.moved)
         }
       } else if (this.dragMode === 'middle') {
-        const container = this.dst.querySelector('.orderable-container')
+        const container = this.target.querySelector('.orderable-container')
         if (container) {
-          container.appendChild(this.src)
+          container.appendChild(this.moved)
         }
       }
     }, 500)
@@ -102,27 +110,21 @@ export default class Orderable {
 
   onDragLeave(event) {
     // event.target is previous target element.
-    const dst = this.findTarget(event.target)
-    if (dst) this.resetCSS(dst)
+    const target = this.findTarget(event.target)
+    if (target) this.resetCSS(target)
   }
 
   onDrop(event) {
     // event.target is current target element.
     if (event.stopPropagation) event.stopPropagation() // Stops the browser from redirecting.
-    if (!this.dst) return
-    this.resetCSS(this.dst)
-    this.onCommit(
-      this.src,
-      this.dst,
-      this.initialIndex,
-      this.nodeIndex(this.src),
-      this.dragMode
-    )
+    if (!this.target) return
+    this.resetCSS(this.target)
+    this.onCommit(this.moved, this.target, this.dragMode)
     return false
   }
 
   onDragEnd(event) {
     // event.target is the source node.
-    this.src.classList.remove('ordering')
+    this.moved.classList.remove('ordering')
   }
 }
