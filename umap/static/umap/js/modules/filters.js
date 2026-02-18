234c9802c4351522ca38206b0d5edc2fe3c7d88f
Yohan Boniface <yohanboniface@free.fr>
2026-02-18 17:53:07 +0100

    wip: use a unique tree for filters management dialog
    Co-authored-by: David Larlet <david@larlet.fr>

--- umap/static/umap/js/modules/filters.js
+++ umap/static/umap/js/modules/filters.js
@@ -267,19 +267,12 @@ export class Filters {
     `
     const body = Utils.loadTemplate(template)
     this._listFilters(this._umap, body, translate('Map (all layers)'))
-    this._umap.layers.root.map((datalayer) => {
-      this._listFilters(
-        datalayer,
-        body,
-        `${datalayer.getName()} (${translate('single layer')})`
-      )
-    })
     this._umap.dialog.open({ template: body })
   }
 
   _listFilters(layer, container, title) {
     const template = Utils.sanitizeVars`
-      <details>
+      <details open>
         <summary>${title}</summary>
         <ul data-ref=ul></ul>
         <div>
@@ -289,15 +282,13 @@ export class Filters {
       </details>
     `
     const [body, { ul, add, childrenContainer }] = Utils.loadTemplateWithRefs(template)
-    if (!layer.filters._parent.fields.size) {
+    if (!layer.fields.size) {
       add.disabled = true
       ul.appendChild(
         Utils.loadTemplate(
           `<li>${translate('Add a field prior to create a filter.')}</li>`
         )
       )
-    } else if (!layer.filters._parent.fields.isDefault()) {
-      body.open = true
     }
     layer.filters.available.forEach((filter, fieldKey) => {
       const [li, { edit, remove }] = Utils.loadTemplateWithRefs(
@@ -324,10 +315,8 @@ export class Filters {
       })
     })
     add.addEventListener('click', () => layer.filters.createFilterForm())
-    if (layer.hasChild?.()) {
-      for (const child of layer.layers.root) {
-        this._listFilters(child, childrenContainer, child.getName())
-      }
+    for (const child of layer.layers.root) {
+      this._listFilters(child, childrenContainer, child.getName())
     }
     const onReorder = (src, dst) => {
       const orderedKeys = Array.from(ul.querySelectorAll('li')).map(
