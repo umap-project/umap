544efa31517ad6450407a0bfe933db3384621a79
Yohan Boniface <yohanboniface@free.fr>
2026-03-09 17:38:44 +0100

    chore: move all views permissions to decorators
    And fix a case where one could cheat and use a map id they own in the
    URL to change some other map datalayer permissions.
    
    Co-authored-by: David Larlet <david@larlet.fr>

--- umap/decorators.py
+++ umap/decorators.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 
-from .models import Map, Team
+from .models import DataLayer, Map, Team
 from .views import simple_json_response
 
 LOGIN_URL = getattr(settings, "LOGIN_URL", "login")
@@ -68,6 +68,56 @@ def team_members_only(view_func):
     def wrapper(request, *args, **kwargs):
         team = get_object_or_404(Team, pk=kwargs["pk"])
         if not request.user.is_authenticated or team not in request.user.teams.all():
+            return HttpResponseForbidden()
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def datalayer_belong_to_map(view_func):
+
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if "map_inst" not in kwargs:
+            map_inst = get_object_or_404(Map, pk=kwargs["map_id"])
+            kwargs["map_inst"] = map_inst  # Avoid rerequesting the map in the view
+        if "datalayer_inst" not in kwargs:
+            datalayer_inst = get_object_or_404(DataLayer, pk=kwargs["pk"])
+            kwargs["datalayer_inst"] = datalayer_inst
+        if kwargs["datalayer_inst"].map != map_inst:
+            return HttpResponseForbidden()
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def can_edit_datalayer(view_func):
+    """
+    Used for URLs dealing with editing a datalayer (user may have rights on the datalayer
+    but not on the map).
+    """
+
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if "datalayer_inst" not in kwargs:
+            datalayer_inst = get_object_or_404(DataLayer, pk=kwargs["pk"])
+            kwargs["datalayer_inst"] = datalayer_inst
+
+        if not kwargs["datalayer_inst"].can_edit(request=request):
+            return HttpResponseForbidden()
+
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def is_anonymous_owner(view_func):
+
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        map_inst = get_object_or_404(Map, pk=kwargs["map_id"])
+        kwargs["map_inst"] = map_inst  # Avoid rerequesting the map in the view
+        if map_inst.owner or not map_inst.is_anonymous_owner(request):
             return HttpResponseForbidden()
         return view_func(request, *args, **kwargs)
 
