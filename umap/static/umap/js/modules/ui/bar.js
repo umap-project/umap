a544aa89fb29fc18479d4c5687383c4c8c04b940
Yohan Boniface <yohanboniface@free.fr>
2026-04-01 18:24:20 +0200

    chore: only add bottomBar to DOM when expected to be visible
    Co-authored-by: David Larlet <david@larlet.fr>

--- umap/static/umap/js/modules/ui/bar.js
+++ umap/static/umap/js/modules/ui/bar.js
@@ -228,7 +228,6 @@ export class BottomBar extends WithTemplate {
   }
 
   setup() {
-    this.parent.appendChild(this.element)
     DomEvent.disableClickPropagation(this.element)
     this._umap.addAuthorLink(this.elements.author)
     this.elements.caption.addEventListener('click', () => this._umap.openCaption())
@@ -261,11 +260,13 @@ export class BottomBar extends WithTemplate {
     const barEnabled = this._umap.properties.captionBar || hasSlideshow
     document.body.classList.toggle('umap-caption-bar-enabled', barEnabled)
     document.body.classList.toggle('umap-slideshow-enabled', hasSlideshow)
+    if (!barEnabled) return
     const showMenus = this._umap.getProperty('captionMenus')
     this.elements.caption.hidden = !showMenus
     this.elements.browse.hidden = !showMenus
     this.elements.filter.hidden = !showMenus || !this._umap.hasFilters()
     this.buildDataLayerSwitcher()
+    this.parent.appendChild(this.element)
   }
 
   buildDataLayerSwitcher() {
