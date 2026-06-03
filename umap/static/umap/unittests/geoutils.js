ea1b66f4f2a9c792c52120fb7e298db618b2395d
Yohan Boniface <yohanboniface@free.fr>
2026-06-03 17:34:51 +0200

    wip: remove Feature.pullGeometry
    Feature had both pushGeometry and pullGeometry, which was not pure
    in terms of flow.
    We first tried to use Turf to convert Leaflet internal's latlngs, but
    those are objects with lat/lng/alt properties, and Turf does not know
    nothing about that.
    So let's make that Feature is geojson only and the UI deals with
    latlngs.
    There are still other coupling, but that's one less!

--- umap/static/umap/unittests/geoutils.js
+++ umap/static/umap/unittests/geoutils.js
@@ -6,11 +6,22 @@ const { assert } = pkg
 describe('GeoUtils', () => {
   describe('#isFlat()', () => {
     it('should return true for GeoJSON LineString coordinates', () => {
-      assert.isTrue(GeoUtils.isFlat([[1, 2], [3, 4], [5, 6]]))
+      assert.isTrue(
+        GeoUtils.isFlat([
+          [1, 2],
+          [3, 4],
+          [5, 6],
+        ])
+      )
     })
 
     it('should return true for an array of Leaflet LatLng-like objects', () => {
-      assert.isTrue(GeoUtils.isFlat([{ lat: 1, lng: 2 }, { lat: 3, lng: 4 }]))
+      assert.isTrue(
+        GeoUtils.isFlat([
+          { lat: 1, lng: 2 },
+          { lat: 3, lng: 4 },
+        ])
+      )
     })
 
     it('should return true for a single position', () => {
@@ -26,22 +37,59 @@ describe('GeoUtils', () => {
     })
 
     it('should return false for GeoJSON MultiLineString coordinates', () => {
-      assert.isFalse(GeoUtils.isFlat([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]))
+      assert.isFalse(
+        GeoUtils.isFlat([
+          [
+            [1, 2],
+            [3, 4],
+          ],
+          [
+            [5, 6],
+            [7, 8],
+          ],
+        ])
+      )
     })
 
     it('should return false for GeoJSON Polygon coordinates (ring of positions)', () => {
-      assert.isFalse(GeoUtils.isFlat([[[1, 2], [3, 4], [5, 6], [1, 2]]]))
+      assert.isFalse(
+        GeoUtils.isFlat([
+          [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [1, 2],
+          ],
+        ])
+      )
     })
 
     it('should return false for GeoJSON MultiPolygon coordinates', () => {
-      assert.isFalse(GeoUtils.isFlat([[[[1, 2], [3, 4], [5, 6], [1, 2]]]]))
+      assert.isFalse(
+        GeoUtils.isFlat([
+          [
+            [
+              [1, 2],
+              [3, 4],
+              [5, 6],
+              [1, 2],
+            ],
+          ],
+        ])
+      )
     })
 
     it('should return false for a nested array of LatLng-like objects (Multi)', () => {
       assert.isFalse(
         GeoUtils.isFlat([
-          [{ lat: 1, lng: 2 }, { lat: 3, lng: 4 }],
-          [{ lat: 5, lng: 6 }, { lat: 7, lng: 8 }],
+          [
+            { lat: 1, lng: 2 },
+            { lat: 3, lng: 4 },
+          ],
+          [
+            { lat: 5, lng: 6 },
+            { lat: 7, lng: 8 },
+          ],
         ])
       )
     })
